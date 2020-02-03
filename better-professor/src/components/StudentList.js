e4e4cea40866d4ac6280344b5ec7fe7a7a75e7ac
Kaela M Gallo <kaelamgallo@Kaelas-MBP.home>
2020-02-03 14:08:13 -0500

    student-list

---
+++ better-professor/src/components/StudentList.js
@@ -0,0 +1,13 @@
+import React, { useEffect, useState } from "react";
+import {
+  card,
+  CardImg,
+  CardText,
+  CardBody,
+  CardTitle,
+  Button
+} from "reactstrap";
+
+const StudentList = () => {
+  const [studentList, setStudentList] = useState([]);
+};
