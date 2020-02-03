1b1ec69f4d456a0c0caee47b9a73d074507937fe
Kaela M Gallo <kaelamgallo@Kaelas-MBP.home>
2020-02-03 16:36:02 -0500

    updated studentlist

--- better-professor/src/components/StudentList.js
+++ better-professor/src/components/StudentList.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from "react";
+import { axiosWithAuth } from "../utils/axiosWithAuth";
+import { Link } from "react-router-dom";
 import {
-  card,
+  Card,
   CardImg,
   CardText,
   CardBody,
@@ -9,5 +11,38 @@ import {
 } from "reactstrap";
 
 const StudentList = () => {
-  const [studentList, setStudentList] = useState([]);
+  const [studentState, setStudentState] = useState([]);
+
+  useEffect(() => {
+    axiosWithAuth()
+      .get("/students")
+      .then(res => {
+        setStudentstate(res.data);
+      })
+      .catch(err => console.log(err));
+  }, []);
+
+  return (
+    <div>
+      <div>
+        {studentState.map(student => {
+          return (
+            <Card key={student.id}>
+              <CardImg src={student.image_url} alt="card-group" />
+              <CardBody>
+                <CardTitle>Name:{student.name}</CardTitle>
+                <CardText>Email:{student.email}</CardText>
+                <Button>Add Project</Button>
+              </CardBody>
+            </Card>
+          );
+        })}
+        <Link>
+          <Button> Add Student </Button>
+        </Link>
+      </div>
+    </div>
+  );
 };
+
+export default StudentList;
