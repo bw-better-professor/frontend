78e54aec7e971cce7e8b2b9ee4f52e495c587304
Kaela M Gallo <kaelamgallo@Kaelas-MBP.home>
2020-02-03 21:48:54 -0500

    made some updates

--- better-professor/src/App.js
+++ better-professor/src/App.js
@@ -2,16 +2,17 @@ import React from "react";
 
 import { Switch, Route, Redirect } from "react-router-dom";
 import styled from "styled-components";
-
+import LoginBasic from "./components/LoginBasic";
+import RegisterBasic from "./components/RegisterBasic";
 import PrivateRoute from "./components/PrivateRoute";
-import Login from "./routes/Login";
-import Register from "./routes/Register";
+// import Register from "./routes/Register";
 
 function App() {
   return (
     <>
       {/* <Wrapper> */}
-      <Login />
+      <LoginBasic />
+      <RegisterBasic />
 
       {/* <Switch>
         <Route exact path="/register">
