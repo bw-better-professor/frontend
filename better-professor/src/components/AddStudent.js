9f2240f1aa060c01cf311b5bef55fc08f6746bc3
Phong Yang <phongyang88@hotmail.com>
2020-02-03 20:05:21 -0600

    added a baSic login form and a basic register form for testing purposes. added a form for adding a student and a project

--- better-professor/src/components/AddStudent.js
+++ better-professor/src/components/AddStudent.js
@@ -1,52 +1,65 @@
 import React from "react";
 import {useForm} from "react-hook-form";
+import {useHistory} from "react-router-dom";
+
 import {axiosWithAuth} from "../utils/axiosWithAuth";
 
 const AddStudent = () => {
         const { handleSubmit, register, errors } = useForm();
+        const history = useHistory();
 
         const onSubmit = values => {
             console.log(values)
-            // axiosWithAuth()
-            // .post(`/api/students`, {
-            //     name: values.name, 
-            //     email: values.email, 
-            //     professor_id: localStorage.getItem(professorID)})
-            // .then(res => {
-            //     console.log(res)
-            // })
-            // .catch(err => {
-            //     console.log(err)
-            // })
+            const professorID = parseInt(localStorage.getItem("professorID"));
+            console.log(professorID)
+            axiosWithAuth()
+            .post(`/api/students`, {
+                professor_id: professorID,
+                name: values.name, 
+                email: values.email
+                })
+            .then(res => {
+                console.log("add student successfull");
+                history.push("/dashboard")
+
+            })
+            .catch(err => {
+                console.log(err)
+            })
         }
 
     return (
-        <form onSubmit={handleSubmit(onSubmit)}>
-            <input
-                name="email"
-                placeholder="email"
-                ref={register({
-                required: 'Required',
-                pattern: {
-                    value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
-                    message: "invalid email address"
-                }
-                })}
-            />
-            {errors.email && errors.email.message}
+        <>
+        {localStorage.getItem("professorID") && localStorage.getItem("token") && (
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <input
+                    name="email"
+                    placeholder="email"
+                    ref={register({
+                    required: 'Required',
+                    pattern: {
+                        value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
+                        message: "invalid email address"
+                    }
+                    })}
+                />
+                {errors.email && errors.email.message}
 
-            <input
-                name="name"
-                placeholder="name"
-                ref={register({
-                    required: "Required",
-                    validate: value => value !== "admin" || "Nice try!"
-                })}
-            />
-            {errors.username && errors.username.message}
+                <input
+                    name="name"
+                    placeholder="name"
+                    ref={register({
+                        required: "Required",
+                        validate: value => value !== "admin" || "Nice try!"
+                    })}
+                />
+                {errors.name && errors.name.message}
 
-            <button type="submit">Add Student</button>
-        </form>
+                <button type="submit">Add Student</button>
+            </form>
+        )}
+        
+        </>
     )
 }
 
